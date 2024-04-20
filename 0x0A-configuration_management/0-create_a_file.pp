6784ac2e3e35ee7bce2e13f222f3d87dff2f0def
root <DerrickWawerunMuturi>
2024-04-20 08:51:38 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp/school' :
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data'
-  group   => 'www-data'
-  content => 'I love Puppet',
+# create a file located at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
