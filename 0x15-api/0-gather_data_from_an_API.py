18b2cb86033b622c98692b2a4386fb3cba727a37
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-21 18:43:11 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
-""" using API in python"""
+""" using API in python
+used a rest api
+"""
 
 
 if __name__ == "__main__":
-    """ used a rest api"""
-    
     import requests
     import sys
 
-    # getting value from terminal
     if len(sys.argv) < 2:
         print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
-    else:
-        id = int(sys.argv[1])
 
-    # main url
+    id = int(sys.argv[1])
     URL = "https://jsonplaceholder.typicode.com/"
+    param = {"userId": id}
 
-    # params
-    param = {
-        "userId": id
-    }
-    # get the  user
     user = requests.get(URL + "/users", params=param)
     user = user.json()
     employee_name = user[id].get('name')
 
-    # get todos
     todos = requests.get(URL + "/todos", params=param).json()
 
-    # ratio of completed tasks
     completed_tasks = [task['title'] for task in todos if task['completed']]
     completed_count = len(completed_tasks)
     total_task = len(todos)
 
-    # output the results
     print(
         f"Employee {employee_name} is done with tasks(
             {completed_count}/{total_task})")
 
-    # task title
     for task in completed_tasks:
         print(f"\t {task}")
