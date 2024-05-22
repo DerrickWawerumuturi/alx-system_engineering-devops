fe1fa3dc09451c86cc499fef46d9bd033fcf55f5
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-05-22 08:09:40 +0300

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     completed_count = len(completed_tasks)
     total_task = len(todos)
 
-    print(f"Employee {employee_name} is done with tasks(
-    {completed_count}/{total_task}):")
+    print(
+        f"Employee {employee_name} is done with tasks({completed_count}/{total_task}):")
     for task in completed_tasks:
         print(f"\t {task}")
