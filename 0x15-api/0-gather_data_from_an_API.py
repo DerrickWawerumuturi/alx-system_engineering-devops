05f5a3f4304b422d6b354a1db668f0e79af92462
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-05-22 08:05:52 +0300

    module is documented

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-""" script that, using a REST API, for a given employee ID,
-returns information about his/her todo list progress."""
+""" Script that, using a REST API, for a given employee ID,
+returns information about his/her todo list progress.
+"""
+
+import requests
+import sys
 
 if __name__ == "__main__":
-    """ required modules """
-    import requests
-    import sys
-    
     if len(sys.argv) < 2:
         print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    id = int(sys.argv[1])
+    employee_id = sys.argv[1]
     URL = "https://jsonplaceholder.typicode.com/"
-    param = {"userId": id}
 
-    user = requests.get(URL + "/users", params=param).json()
-    employee_name = user[id].get('name')
-    todos = requests.get(URL + "/todos", params=param).json()
+    user_response = requests.get(f"{URL}users/{employee_id}")
+    if user_response.status_code != 200:
+        print("Error: User not found")
+        sys.exit(1)
+
+    user = user_response.json()
+    employee_name = user.get('name')
+
+    todos_response = requests.get(f"{URL}todos", params={"userId": employee_id})
+    if todos_response.status_code != 200:
+        print("Error: Todos not found")
+        sys.exit(1)
+
+    todos = todos_response.json()
 
     completed_tasks = [task['title'] for task in todos if task['completed']]
     completed_count = len(completed_tasks)
     total_task = len(todos)
 
-    print(
-        f"Employee {employee_name} is done with tasks(
-            {completed_count}/{total_task})")
-
+    print(f"Employee {employee_name} is done with tasks({completed_count}/{total_task}):")
     for task in completed_tasks:
         print(f"\t {task}")
