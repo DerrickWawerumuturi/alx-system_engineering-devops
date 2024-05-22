cc3cbbc870af27f29a17fb7be610a3c7e9ce829b
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-22 09:16:37 +0300

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Script that, using a REST API, for a given employee ID,
+returns information about his/her todo list progress.
+"""
+import pandas as pd
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    URL = "https://jsonplaceholder.typicode.com/"
+
+    user_response = requests.get(f"{URL}users/{employee_id}")
+    if user_response.status_code != 200:
+        print("Error: User not found")
+        sys.exit(1)
+
+    user = user_response.json()
+    employee_name = user.get('name')
+
+    todos_response = requests.get(f"{URL}todos",
+                                  params={"userId": employee_id})
+    if todos_response.status_code != 200:
+        print("Error: Todos not found")
+        sys.exit(1)
+
+    todos = todos_response.json()
+    completed_status = [task['completed'] for task in todos]
+    task_title = [task['title'] for task in todos]
+    total_task = len(todos)
+
+    record = "{}, {}, {},{}".format(
+        employee_id, employee_name, completed_status,
+        task_title 
+    )
+    
+    df = pd.DataFrame(record)
+    print(df)
+    
+    df.to_csv(r"USER_ID.csv", index=False)
+
