c44c8eee5cb8ed437605bbe5ffa4755f8816dc27
Derrick Muturi <wawerumuturi57@gmail.com>
2024-06-04 15:52:31 +0300

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,3 @@
-# Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-
-# Requirements:
-
-# Prototype: def top_ten(subreddit)
-# If not a valid subreddit, print None.
-# NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
-
 #!/usr/bin/python3
 import requests
 """ get requests"""
@@ -19,7 +11,7 @@ def top_ten(subreddit):
         params={"limit:10"},
         allow_redirects=False
     )
-    
+
     if res.status_code == 200:
         for top in res.json().get("data").get("children"):
             res_data = top.get("data")
