9b7671e48d5ba0b9f92e5c7bb571a7dec1576dc4
Derrick Muturi <wawerumuturi57@gmail.com>
2024-06-04 18:31:39 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number
-of subscriber for a given subreddit
+queries the Reddit API and returns the number of subscribers (not active users,
+total subscribers) for a given subreddit. If an invalid subreddit is given,
+return 0
 """
-
-import requests
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+    """returns number of subscribers for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/about.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
+    try:
+        subreddit_dict = r_subreddit.json()
+        return subreddit_dict['data']['subscribers']
+    except:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""first 10 hot posts listed for a given subreddit"""
-import requests
+"""
+queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit
+"""
+from requests import get
 
 
 def top_ten(subreddit):
-    """ function prints titles of the first 10
-    posts or None if subreddit is invalid
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Get 10 hottest post'}
-    params = {'limit': '10'}
-
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params)
-
-    if response.status_code == 200:
-        for posts in response.json().get('data').get('children'):
-            post = posts.get('data')
-            title = post.get('title')
-            print(title)
-    else:
+    """prints titles of first 10 hot posts listed for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/hot.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
+    try:
+        subreddit_dict = r_subreddit.json()
+        for i in range(10):
+            print(subreddit_dict['data']['children'][i]['data']['title'])
+    except:
         print(None)
