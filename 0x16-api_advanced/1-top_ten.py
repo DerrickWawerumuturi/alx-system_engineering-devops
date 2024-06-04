1475edead955552190cad8b491864e776722e137
Derrick Muturi <wawerumuturi57@gmail.com>
2024-06-04 17:46:41 +0300

    pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,14 @@ import json
 import requests
 import sys
 
+
 def top_ten(subreddit):
     """ function prints titles of the first 10
     posts or None if subreddit is invalid
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Get 10 hottest post'}
-    params = {'limit':' 10'}
+    params = {'limit': '10'}
 
     response = requests.get(
         url,
@@ -18,10 +19,9 @@ def top_ten(subreddit):
         params=params)
 
     if response.status_code == 200:
-        for posts in  response.json().get('data').get('children'):
+        for posts in response.json().get('data').get('children'):
             post = posts.get('data')
             title = post.get('title')
             print(title)
     else:
         print(None)
- 
