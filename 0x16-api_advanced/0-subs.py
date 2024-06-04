7ec477cc346adf69eef3d31ae66a5596c7b98f62
Derrick Muturi <wawerumuturi57@gmail.com>
2024-06-04 14:56:16 +0300

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 import requests
 """ using reddit api"""
 
+
 def number_of_subscribers(subreddit):
     """Function to get the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
